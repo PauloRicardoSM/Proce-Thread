c68f8034afdd662ca829ed219d3f3abb476ff3ca
Eros-afk <erosryansimette@alu.ufc.br>
2025-06-13 16:09:51 -0300

    #

--- Processos.c
+++ Processos.c
@@ -1,10 +1,36 @@
-#include <unistd.h>  
-#include <stdio.h>  
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main() {  
-    printf("Processo PAI (PID: %d)\n", getpid());  
-    fork();  // Cria um filho  
-    printf("Quem sou eu? PID: %d\n", getpid());  
-    sleep(1);  // Pausa para ver a saída  
-    return 0;  
-}  
+int main() {
+    pid_t pid;
+
+    printf("Processo pai (PID: %d) iniciando.\\n", getpid());
+
+    pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Falha no fork.\\n");
+        return 1;
+    } else if (pid == 0) {
+        // Processo filho
+        printf("Processo filho (PID: %d) criado. "
+               "Meu pai é (PID: %d).\\n", 
+               getpid(), getppid());
+        sleep(2); // Simula algum trabalho
+        printf("Processo filho (PID: %d) terminando.\\n", 
+               getpid());
+        exit(0);
+    } else {
+        // Processo pai
+        printf("Processo pai (PID: %d) criou o filho "
+               "(PID: %d).\\n", getpid(), pid);
+        wait(NULL); // Espera o processo filho terminar
+        printf("Processo pai (PID: %d) detectou que o "
+               "filho terminou. Terminando também.\\n", 
+               getpid());
+    }
+
+    return 0;
+}
--- Threads.c
+++ Threads.c
@@ -1,23 +1,43 @@
-#include <pthread.h>  
-#include <stdio.h>  
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-int counter = 0;  
-pthread_mutex_t lock;  
+// Função que será executada pela thread
+void *minhaThread(void *arg) {
+    int id = *((int *)arg);
+    printf("Thread %d: Olá do mundo das threads!\\n", id);
+    sleep(1); // Simula algum trabalho
+    printf("Thread %d: Terminando.\\n", id);
+    pthread_exit(NULL);
+}
 
-void* increment(void* arg) {  
-    pthread_mutex_lock(&lock);  
-    counter++;  // Seção crítica!  
-    pthread_mutex_unlock(&lock);  
-    return NULL;  
-}  
+int main() {
+    pthread_t thread1, thread2;
+    int id1 = 1;
+    int id2 = 2;
 
-int main() {  
-    pthread_t t1, t2;  
-    pthread_mutex_init(&lock, NULL);  
-    pthread_create(&t1, NULL, increment, NULL);  
-    pthread_create(&t2, NULL, increment, NULL);  
-    pthread_join(t1, NULL);  
-    pthread_join(t2, NULL);  
-    printf("Counter: %d\n", counter);  // Sem mutex, pode ser 1 ou 2!  
-    return 0;  
-}  
+    printf("Processo principal: Criando thread 1.\\n");
+    if (pthread_create(&thread1, NULL, minhaThread, 
+                       (void *)&id1) != 0) {
+        fprintf(stderr, "Erro ao criar thread 1.\\n");
+        return 1;
+    }
+
+    printf("Processo principal: Criando thread 2.\\n");
+    if (pthread_create(&thread2, NULL, minhaThread, 
+                       (void *)&id2) != 0) {
+        fprintf(stderr, "Erro ao criar thread 2.\\n");
+        return 1;
+    }
+
+    printf("Processo principal: Esperando as threads "
+           "terminarem.\\n");
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Processo principal: Todas as threads "
+           "terminaram. Encerrando.\\n");
+
+    return 0;
+}
